b45f5ec8a6b222a2c972217f970c037d0f017430
T-Nicholls <tobyn.nicholls@diamond.ac.uk>
2018-08-23 17:13:05 +0100

    Update atip to comply with new pytac class formats.

--- atip/SimulatorModel.py
+++ atip/SimulatorModel.py
@@ -4,15 +4,15 @@ import pytac
 from at import physics
 from pytac.exceptions import FieldException, HandleException
 
-class ATModel(object):
+class ATElementModel(object):
     def __init__(self, at_element, at_interface, fields=[]):
         self.field_functions = {'a0' : partial(self.PolynomA, cell=0),
                                 'a1' : partial(self.PolynomA, cell=1),
                                 'b0' : partial(self.PolynomB, cell=0),
                                 'b1' : partial(self.PolynomB, cell=1),
                                 'b2' : partial(self.PolynomB, cell=2),
-                                'x' : partial(self.Orbit, cell=0),
-                                'y' : partial(self.Orbit, cell=2),
+                                'x' : partial(self.Orbit, field='x'),
+                                'y' : partial(self.Orbit, field='y'),
                                 'f' : self.Frequency}
         self.units = pytac.PHYS #conversion is done in element.(set/get)_value before and after pass
         self.at = at_interface
@@ -31,6 +31,9 @@ class ATModel(object):
         else:
             raise FieldException("No field {} on AT element {}".format(field, self._element))
     
+        def get_fields(self):
+            return set(self._fields)
+    
     def PolynomA(self, cell, value): #use value as get/set flag as well as the set value
         if numpy.isnan(value):
             return self._element.PolynomA[cell]
@@ -45,10 +48,10 @@ class ATModel(object):
             self._element.PolynomB[cell] = value
             self.at.push_changes(self._element)
     
-    def Orbit(self, cell, value):
+    def Orbit(self, field, value):
         index = self._element.Index-1
         if numpy.isnan(value):
-            return float(physics.find_orbit4(self.at.pull_ring(), refpts=index)[1][cell])
+            return float(self.at.get_value(field)[index])
         else:
             raise HandleException("Must read beam position using {}".format(pytac.RB))
     
@@ -58,6 +61,52 @@ class ATModel(object):
         else:
             self._element.Frequency = value
             self.at.push_changes(self._element)
+
+
+class ATLatticeModel(object):
+    def __init__(self, ring):
+        self.ring = ring
+        #temporary work around for AT None bug:
+        self.rp = []
+        for x in range(len(self.ring)):
+            self.rp.append(x)
+        #work around end
+        self.twiss = physics.get_twiss(self.ring, refpts=self.rp, get_chrom=True)
+        self.field2twiss = {'x' : partial(self.read_twiss, cell=0, field='closed_orbit', limiter=0),
+                            'px' : partial(self.read_twiss, cell=0, field='closed_orbit', limiter=1),
+                            'y' : partial(self.read_twiss, cell=0, field='closed_orbit', limiter=2),
+                            'py' : partial(self.read_twiss, cell=0, field='closed_orbit', limiter=3),
+                            'm44' : partial(self.read_twiss, cell=0, field='m44', limiter=None),
+                            'idx' : partial(self.read_twiss, cell=0, field='idx', limiter=None),
+                            's_pos' : partial(self.read_twiss, cell=0, field='s_pos', limiter=None),
+                            'alpha' : partial(self.read_twiss, cell=0, field='alpha', limiter=None),
+                            'beta' : partial(self.read_twiss, cell=0, field='beta', limiter=None),
+                            'mu' : partial(self.read_twiss, cell=0, field='mu', limiter=None),
+                            'dispersion' : partial(self.read_twiss, cell=0, field='dispersion', limiter=None),
+                            'tune' : partial(self.read_twiss, cell=1, field=None, limiter=None),
+                            'chrom' : partial(self.read_twiss, cell=2, field=None, limiter=None)}
+
+    
+    def get_value(self, field):
+        if field in self.field2twiss.keys():
+            self.twiss = physics.get_twiss(self.ring, refpts=self.rp, get_chrom=True)
+            return self.field2twiss[field]()
+        else:
+            raise FieldException('Lattice model {} does not have field {}'.format(self, field))
+    
+    def set_value(self, field):
+        raise HandleException('Filed {} cannot be set on lattice model {}'.format(field, self))
     
     def get_fields(self):
-        return self._fields
+        return set(self.field2twiss.keys())
+    
+    def read_twiss(self, cell, field, limiter):
+        if field==None:
+            return self.twiss[cell]
+        elif limiter==None:
+            return self.twiss[cell][field]
+        else:
+            return self.twiss[cell][field][:,limiter]
+    
+    def push_changes(self, element):
+        self.ring[element.Index-1] = element
--- atip/load_sim.py
+++ atip/load_sim.py
@@ -1,23 +1,15 @@
 import pytac
 from at import load_mat
-from SimulatorModel import ATModel
+from SimulatorModel import ATElementModel, ATLatticeModel
 
 def load(lattice, LATTICE_FILE=None):
     if LATTICE_FILE is None:
         LATTICE_FILE = './vmx.mat'
-    AT = at_interface(LATTICE_FILE)
     ring = load_mat.load(LATTICE_FILE)
+    at_interface = ATLatticeModel(ring)
     for x in range(len(ring)):
         ring[x].Index = x+1
         ring[x].Class = ring[x].__doc__.split()[1] #This ensures all elems have a class but likely will not work for other .mat files
     for e in lattice:
-        e.set_model(ATModel(ring[e.index-1], AT, e.get_fields()), pytac.SIM) #could be combined with the above loop - is the clarity of separation worth the extra loop
+        e.set_model(ATElementModel(ring[e.index-1], at_interface, e.get_fields()), pytac.SIM)
     return lattice
-
-class at_interface(object):
-    def __init__(self, LATTICE_FILE):
-        self.ring = load_mat.load(LATTICE_FILE)
-    def push_changes(self, element):
-        self.ring[element.Index-1] = element
-    def pull_ring(self):
-        return self.ring
