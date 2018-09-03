b737fb25fd5e712a619064acd694fb361427cebb
T-Nicholls <tobyn.nicholls@diamond.ac.uk>
2018-09-03 15:39:39 +0100

    Changes to the importing in __init__.py.

--- atip/__init__.py
+++ atip/__init__.py
@@ -17,5 +17,8 @@ sys.path.append(source_directory+'/pytac')
 sys.path.append(source_directory+'/at/pyat/build/lib.linux-x86_64-2.7')
 
 # Initialise all modules.
-import pytac, at  # noqa: E401,F401
-from . import load_sim, SimDataSource, ease  # noqa: E402,F401
+from . import load_sim, SimDataSource, ease  # noqa: E402
+"""Error 402 is suppressed as we cannot import these modules at the top of the
+file as pytac and at must be added to the path first or the imports will fail.
+"""
+__all__ = ["load_sim", "SimDataSource", "ease"]
