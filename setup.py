fdb5580bcf8d1ee31aeeb2c2c843627e271011f4
T-Nicholls <tobyn.nicholls@diamond.ac.uk>
2018-10-17 19:53:47 +0100

    Groundwork for pip install atip.

---
+++ setup.py
@@ -0,0 +1,92 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+# io.open is needed for projects that support Python 2.7
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.mrst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='atip',
+
+    version='0.0.1',
+
+    description='ATIP: Accelerator Toolbox Interface for Pytac',
+
+    long_description=long_description,
+
+    url='https://github.com/T-Nicholls/atip',
+
+    author='Tobyn Nicholls',
+
+    author_email='tobyn.nicholls@diamond.ac.uk',
+
+    license='Apache License 2.0',
+
+    # For a list of valid classifiers, see https://pypi.org/classifiers/
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='accelerator physics',
+
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    #packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages = ['atip']
+
+    include_package_data=True,
+
+    zip_safe=False,
+
+    install_requires=['pytac, at-python'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). Users will be able to install these using the "extras"
+    # syntax, for example:
+    #
+    #   $ pip install sampleproject[dev]
+    #
+    # Similar to `install_requires` above, these must be valid existing
+    # projects.
+    extras_require={  # Optional
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+)
