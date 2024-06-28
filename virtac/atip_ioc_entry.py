17869a6150fe1025319452351440635c76b87212
T-Nicholls <tobyn.nicholls@diamond.ac.uk>
2024-06-28 16:33:46 +0100

    Warn if virtac is started on the wrong port

--- virtac/atip_ioc_entry.py
+++ virtac/atip_ioc_entry.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 import os
 from pathlib import Path
+from warnings import warn
 
 import epicscorelibs.path.cothread  # noqa
 from cothread.catools import ca_nothing, caget
@@ -59,6 +60,35 @@ def main():
     # Add special case out record for SOFB to write to.
     builder.SetDeviceName("CS-CS-MSTAT-01")
     builder.aOut("FBHEART", initial_value=10)
+
+    # Warn if set to default EPICS port(s) accounting for env var inheritance/fallback.
+    conflict_warning = ", this may lead to conflicting PV names on multiple servers."
+    epics_env_vars = [
+        "EPICS_CA_REPEATER_PORT",
+        "EPICS_CAS_SERVER_PORT",
+        "EPICS_CA_SERVER_PORT",
+        "EPICS_CAS_BEACON_PORT",
+    ]
+    # Need 0 & 1 to both be set or just 2 set, but none of the others can be set wrong.
+    if (
+        bool(set(epics_env_vars[:2]) - os.environ.keys())
+        or epics_env_vars[2] not in os.environ.keys()
+    ):
+        warn(
+            "No EPICS port set, default base port (5064) will be used"
+            + conflict_warning
+        )
+    ports_list = [int(os.environ.get(env_var, 0)) for env_var in epics_env_vars]
+    if 5064 in ports_list:
+        warn(
+            "'EPICS_CA_SERVER_PORT' or 'EPICS_CAS_SERVER_PORT' is set to 5064"
+            + conflict_warning
+        )
+    elif 5065 in ports_list:
+        warn(
+            "'EPICS_CA_REPEATER_PORT' or 'EPICS_CAS_BEACON_PORT' is set to 5065"
+            + conflict_warning
+        )
 
     # Start the IOC.
     builder.LoadDatabase()
