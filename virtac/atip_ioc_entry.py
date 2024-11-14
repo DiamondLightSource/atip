38edc07736dd5126b5d07e5186e00712beae1f18
T-Nicholls <tobyn.nicholls@diamond.ac.uk>
2024-11-14 12:20:55 +0000

    Make disable_emittance actually disable it and update help info

--- virtac/atip_ioc_entry.py
+++ virtac/atip_ioc_entry.py
@@ -19,21 +19,29 @@ DATADIR = Path(__file__).absolute().parent / "data"
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("ring_mode", nargs="?", type=str, help="Ring mode name")
     parser.add_argument(
-        "--disable-emittance",
+        "ring_mode",
+        nargs="?",
+        type=str,
+        help="The ring mode to be used, e.g., IO4 or DIAD",
+    )
+    parser.add_argument(
         "-d",
-        help="disable the simulator's time-consuming emittance calculation",
+        "--disable-emittance",
+        help="Disable the simulator's time-consuming emittance calculation",
         action="store_true",
     )
     parser.add_argument(
-        "--enable-tfb",
         "-t",
-        help="simulate extra dummy hardware to be used by the Tune Feedback system",
+        "--enable-tfb",
+        help="Simulate extra dummy hardware to be used by the Tune Feedback system",
         action="store_true",
     )
     parser.add_argument(
-        "--verbose", "-v", help="increase output verbosity", action="store_true"
+        "-v",
+        "--verbose",
+        help="Increase output and logging verbosity",
+        action="store_true",
     )
     return parser.parse_args()
 
@@ -63,7 +71,7 @@ def main():
         DATADIR / ring_mode / "feedback.csv",
         DATADIR / ring_mode / "mirrored.csv",
         DATADIR / ring_mode / "tunefb.csv",
-        not args.disable_emittance,
+        args.disable_emittance,
     )
 
     # Warn if set to default EPICS port(s) as this will likely cause PV conflicts.
