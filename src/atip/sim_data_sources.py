a0699bce815e4d6653a3e3a92d0e539805e64528
Phil Smith <pts@observatorysciences.co.uk>
2025-09-11 10:40:55 +0100

    Handle KickAngle for multipoles attached to correctors

--- src/atip/sim_data_sources.py
+++ src/atip/sim_data_sources.py
@@ -202,13 +202,13 @@ class ATElementDataSource(pytac.data_source.DataSource):
         """A data handling function used to get the value of a specific cell
         of the KickAngle attribute of the AT element.
 
-        .. Note:: If the Corrector is attached to a Sextupole then KickAngle
-           needs to be returned from cell 0 of the applicable Polynom(A/B)
-           attribute and so a conversion must take place. For independent
-           Correctors KickAngle can be returned directly from the element's
-           KickAngle attribute without any conversion. This is because
-           independent Correctors have a KickAngle attribute in our AT lattice,
-           but those attached to Sextupoles do not.
+        .. Note:: If the Corrector is attached to a Sextupole or octupole then
+            KickAngle needs to be returned from cell 0 of the applicable Polynom(A/B)
+            attribute and so a conversion must take place. For independent
+            Correctors KickAngle can be returned directly from the element's
+            KickAngle attribute without any conversion. This is because
+            independent Correctors have a KickAngle attribute in our AT lattice,
+            but those attached to Sextupoles do not.
 
         Args:
             cell (int): Which cell of KickAngle to get.
@@ -216,7 +216,7 @@ class ATElementDataSource(pytac.data_source.DataSource):
         Returns:
             float: The kick angle of the specified cell.
         """
-        if isinstance(self._at_element, at.elements.Sextupole):
+        if isinstance(self._at_element, (at.elements.Sextupole, at.elements.Multipole)):
             length = self._at_element.Length
             if cell == 0:
                 return -(self._at_element.PolynomB[0] * length)
@@ -229,19 +229,19 @@ class ATElementDataSource(pytac.data_source.DataSource):
         """A data handling function used to set the value of a specific cell
         of the KickAngle attribute of the AT element.
 
-        .. Note:: If the Corrector is attached to a Sextupole then KickAngle
-           needs to be assigned to cell 0 of the applicable Polynom(A/B)
-           attribute and so a conversion must take place. For independent
-           Correctors KickAngle can be assigned directly to the element's
-           KickAngle attribute without any conversion. This is because
-           independent Correctors have a KickAngle attribute in our AT lattice,
-           but those attached to Sextupoles do not.
+        .. Note:: If the Corrector is attached to a Sextupole  or ocupole then
+            KickAngle needs to be assigned to cell 0 of the applicable Polynom(A/B)
+            attribute and so a conversion must take place. For independent
+            Correctors KickAngle can be assigned directly to the element's
+            KickAngle attribute without any conversion. This is because
+            independent Correctors have a KickAngle attribute in our AT lattice,
+            but those attached to Sextupoles do not.
 
         Args:
             cell (int): Which cell of KickAngle to set.
             value (float): The angle to be set.
         """
-        if isinstance(self._at_element, at.elements.Sextupole):
+        if isinstance(self._at_element, (at.elements.Sextupole, at.elements.Multipole)):
             length = self._at_element.Length
             if cell == 0:
                 self._at_element.PolynomB[0] = -(value / length)
