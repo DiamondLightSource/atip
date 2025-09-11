9a2fc04ec5f3c5889bd1753679552fb40b07c98b
Phil Smith <pts@observatorysciences.co.uk>
2025-09-11 10:23:09 +0100

    Add octupole field, b3 to supported fields

--- src/atip/load_sim.py
+++ src/atip/load_sim.py
@@ -34,6 +34,7 @@ def load_from_filepath(
         name=pytac_lattice.name,
         energy=pytac_lattice.get_value("energy", units=pytac.PHYS),
     )
+    at_lattice.disable_6d()
     return load(pytac_lattice, at_lattice, callback, disable_emittance)
 
 
